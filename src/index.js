fdff667a77cf5441b5aaf72fc9ba70de38ffde76
Balazs Gobel <balazs.gobel@tretton37.com>
2016-09-27 06:49:08 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='app'>
+
+      </div>
+    )
+  }
+}
